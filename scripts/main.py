d4dd800ec398dda341b315991367c5d433ccc579
ktierney15 <kevintierney99@gmail.com>
2025-07-16 19:19:00 -0400

    log cost data

--- scripts/main.py
+++ scripts/main.py
@@ -9,7 +9,8 @@ def main():
     aws_cost_data = aws_requests.generate_cost_summary()
     email_body = emails.create_email_body(aws_cost_data)
     # emails.send_email(email_body, os.getenv("EMAIL_RECIPIENT"), os.getenv("EMAIL_USER"), os.getenv("EMAIL_PASSWORD"))
-    emails.notify("CLOUD MONTHLY COST OVERVIEW", email_body)
+    # emails.notify("CLOUD MONTHLY COST OVERVIEW", email_body)
+    print(email_body)
 
 if __name__ == '__main__':
     load_dotenv()
